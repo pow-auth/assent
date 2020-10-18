ea966d4138628c5177a9fbf7d88899b5a0fe19cb
Dan Schultzer <dan@dreamconception.com>
2020-10-18 11:09:03 -0700

    Refactor and use new id_token_signed_response_alg attribute

--- lib/assent/strategies/line.ex
+++ lib/assent/strategies/line.ex
@@ -12,16 +12,7 @@ defmodule Assent.Strategy.LINE do
     [
       site: "https://access.line.me",
       authorization_params: [scope: "email profile", response_type: "code"],
-      openid_configuration: %{
-        "id_token_signed_response_alg" => ["HS256"],
-        "issuer" => "https://access.line.me",
-        "authorization_endpoint" => "https://access.line.me/oauth2/v2.1/authorize",
-        "token_endpoint" => "https://api.line.me/oauth2/v2.1/token",
-        "jwks_uri" => "https://api.line.me/oauth2/v2.1/certs",
-        "response_types_supported" => ["code"],
-        "subject_types_supported" => ["pairwise"],
-        "id_token_signing_alg_values_supported" => ["ES256"]
-      }
+      id_token_signed_response_alg: "HS256"
     ]
   end
 
--- test/assent/strategies/line_test.exs
+++ test/assent/strategies/line_test.exs
@@ -3,25 +3,22 @@ defmodule Assent.Strategy.LINETest do
 
   alias Assent.Strategy.LINE
 
-  @id_token elem(
-              Assent.Strategy.sign_jwt(
-                %{
-                  "iss" => "https://access.line.me",
-                  "sub" => "U1234567890abcdef1234567890abcdef ",
-                  "aud" => "1234567890",
-                  "exp" => :os.system_time(:second) + 60,
-                  "iat" => :os.system_time(:second),
-                  "nonce" => "0987654asdf",
-                  "amr" => ["pwd"],
-                  "name" => "Taro Line",
-                  "picture" => "https://sample_line.me/aBcdefg123456"
-                },
-                "HS256",
-                "secret",
-                []
-              ),
-              1
-            )
+  # From https://developers.line.biz/en/docs/line-login/integrate-line-login/#verify-id-token
+  @id_token elem(Assent.Strategy.sign_jwt(
+    %{
+      "iss" => "https://access.line.me",
+      "sub" => "U1234567890abcdef1234567890abcdef ",
+      "aud" => "1234567890",
+      "exp" => :os.system_time(:second) + 60,
+      "iat" => :os.system_time(:second),
+      "nonce" => "0987654asdf",
+      "amr" => ["pwd"],
+      "name" => "Taro Line",
+      "picture" => "https://sample_line.me/aBcdefg123456"
+    },
+    "HS256",
+    "secret",
+    []), 1)
   @user %{
     "name" => "Taro Line",
     "picture" => "https://sample_line.me/aBcdefg123456",
@@ -35,17 +32,9 @@ defmodule Assent.Strategy.LINETest do
   end
 
   test "callback/2", %{config: config, callback_params: params, bypass: bypass} do
-    openid_config =
-      Map.merge(config[:openid_configuration], %{"issuer" => "https://access.line.me"})
-
+    openid_config  = Map.merge(config[:openid_configuration], %{"issuer" => "https://access.line.me"})
     session_params = Map.put(config[:session_params], :nonce, "0987654asdf")
-
-    config =
-      Keyword.merge(config,
-        openid_configuration: openid_config,
-        client_id: "1234567890",
-        session_params: session_params
-      )
+    config         = Keyword.merge(config, openid_configuration: openid_config, client_id: "1234567890", session_params: session_params)
 
     expect_oidc_access_token_request(bypass, id_token: @id_token)
 
