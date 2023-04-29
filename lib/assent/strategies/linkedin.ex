25f25ac18eab6c74375765fe79eace7b314e1572
Dan Schultzer <1254724+danschultzer@users.noreply.github.com>
2023-04-29 07:39:46 -0700

    Update Linkedin module doc

--- lib/assent/strategies/linkedin.ex
+++ lib/assent/strategies/linkedin.ex
@@ -1,6 +1,17 @@
 defmodule Assent.Strategy.Linkedin do
   @moduledoc """
-  Sign In with LinkedIn v2 OpenID Connect Strategy.
+  Sign In with LinkedIn V2 OpenID Connect Strategy.
+
+  You'll need the `Sign In with LinkedIn v2` product enabled in your app. See the
+  [LinkedIn integration guide](https://learn.microsoft.com/en-us/linkedin/consumer/integrations/self-serve/sign-in-with-linkedin-v2)
+  for more.
+
+  ## Usage
+
+      config = [
+        client_id: "REPLACE_WITH_CLIENT_ID",
+        client_secret: "REPLACE_WITH_CLIENT_SECRET"
+      ]
 
   See `Assent.Strategy.OIDC` for more.
   """
