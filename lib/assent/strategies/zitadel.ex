c2de27f85b5e6867257a9f10a3350df88689d763
Tommaso Patrizi <tommasop@thinkingco.de>
2024-07-12 13:21:39 +0200

    fixed tests

--- lib/assent/strategies/zitadel.ex
+++ lib/assent/strategies/zitadel.ex
@@ -2,12 +2,28 @@ defmodule Assent.Strategy.Zitadel do
   @moduledoc """
   Zitadel Sign In OIDC strategy.
 
+  ## Needed settings
+
+  Zitadel reccommended authentication implementation is OIDC with PKCE.
+  This means we need to add PKCE management in Oauth2 strategy and also
+  `code_verifier` management in Oidc strategy.
+
+  I added a `none` `client_authentication_method` as per zitadel 
+  authentication method configuration which maps to auth_method nil.
+
+  I also added a resource_id parameter which is needed in Oidc strategy,
+  the Zitadel resource id is the id of the project, it is added by default
+  in the token `auds` and must be added in the trusted sources to avoid 
+  `Untrusted audiences` error.
+
   ## Usage
 
   ### Required configuration parameters
       config = [
         base_url: ""
+        issuer: should be same of base url
         client_id: "REPLACE_WITH_CLIENT_ID",
+        resource_id: "REPLACE_WITH_RESOURCE_ID",
         redirect_uri: "http://localhost:4000/auth/callback",
         response_type: one of code, id_token token, id_token,
         scope: openid is required other options [email, profile]
@@ -22,7 +38,14 @@ defmodule Assent.Strategy.Zitadel do
   @impl true
   def default_config(config) do
     {:ok, base_url} = Config.fetch(config, :base_url)
-    {:ok, issuer} = Config.fetch(config, :issuer)
+    issuer = get_in(config, [:openid_configuration, "issuer"])
+
+    if is_nil(issuer) do
+      {:error, Assent.Config.MissingKeyError.exception(key: "issuer")}
+    end
+
+    client_authentication_method =
+      Config.get(config, :client_authentication_method, "none")
 
     [
       base_url: base_url,
@@ -34,7 +57,7 @@ defmodule Assent.Strategy.Zitadel do
         "token_endpoint_auth_methods_supported" => ["none"]
       },
       authorization_params: [scope: "email", response_type: "code"],
-      client_authentication_method: "none",
+      client_authentication_method: client_authentication_method,
       openid_default_scope: "openid"
     ]
   end
--- test/assent/strategies/zitadel_test.exs
+++ test/assent/strategies/zitadel_test.exs
@@ -3,11 +3,12 @@ defmodule Assent.Strategy.ZitadelTest do
 
   alias Assent.Strategy.Zitadel
 
-  @client_id "nameofproject@3425235252"
+  @client_id "3425235252@nameofproject"
+  @resource_id "3425296767"
   @id_token_claims %{
     "iss" => "https://zitadel.cloud",
     "sub" => "001473.fe6f6f83bf4b8e4590aacbabdcb8598bd0.2039",
-    "aud" => @client_id,
+    "aud" => [@client_id, @resource_id],
     "exp" => :os.system_time(:second) + 5 * 60,
     "iat" => :os.system_time(:second),
     "email" => "john.doe@example.com",
@@ -19,22 +20,20 @@ defmodule Assent.Strategy.ZitadelTest do
   }
 
   setup %{config: config, callback_params: callback_params} do
-    openid_config = %{
+    openid_configuration = %{
       "issuer" => "https://zitadel.cloud",
       "authorization_endpoint" => TestServer.url("/oauth/v2/authorize"),
       "token_endpoint" => TestServer.url("/oauth/v2/token"),
       "userinfo_endpoint" => TestServer.url("/userinfo"),
       "jwks_uri" => TestServer.url("/jwks_uri.json"),
-      "token_endpoint_auth_methods_supported" => ["client_secret_post", nil]
+      "token_endpoint_auth_methods_supported" => ["client_secret_post", "none"]
     }
 
-    openid_configuration =
-      Map.merge(Map.delete(config[:openid_configuration], "issuer"), openid_config)
-
     config = Keyword.put(config, :openid_configuration, openid_configuration)
+    config = Keyword.put(config, :client_authentication_method, "none")
 
     callback_params =
-      Map.merge(callback_params, %{"nonce" => "123523", "code_verifier" => "456856"})
+      Map.merge(callback_params, %{"code" => "123523", "state" => "456856"})
 
     {:ok, config: config, callback_params: callback_params}
   end
@@ -61,23 +60,14 @@ defmodule Assent.Strategy.ZitadelTest do
   test "callback/2", %{config: config, callback_params: params} do
     openid_config =
       config[:openid_configuration]
-      |> Map.put(
-        "issuer",
-        "https://zitadel.cloud"
-      )
-      |> Map.put("token_endpoint_auth_methods_supported", [
-        "client_secret_post",
-        "private_key_jwt",
-        "client_secret_basic"
-      ])
 
-    session_params = Map.put(config[:session_params], :nonce, "123523")
-    session_params = Map.put(session_params, :code_verifier, "456856")
+    session_params = %{nonce: "123523", state: "456856", code_verifier: "ttt333qqq000"}
 
     config =
       Keyword.merge(config,
         openid_configuration: openid_config,
         client_id: @client_id,
+        resource_id: @resource_id,
         session_params: session_params
       )
 
